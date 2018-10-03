197c785b426c7b31fb4be56e205c5a77702651d4
Jonathan Kamens <jik@kamens.us>
2018-10-03 11:48:28 -0400

    Initial revisions

---
+++ dnsbl-check.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+verbose=false
+
+PERL_ADDR_TO_QUERY='
+use English;
+
+while (<>) {
+    chomp;
+    if (/:/) {
+        if (/::/) {
+            $before = $PREMATCH;
+            $after = $POSTMATCH;
+        }
+        else {
+            $before = $_;
+            $after = "";
+        }
+        @before = split(/:/, $before);
+        @after = split(/:/, $after);
+        while (@before + @after < 8) {
+            push(@before, "0");
+        }
+        for (reverse(@before, @after)) {
+            push(@numbers, reverse(split(//, sprintf("%04x", hex($_)))));
+        }
+    }
+    else {
+        @numbers = reverse(split(/\./, $_));
+    }
+    print(join(".", @numbers), "\n");
+}
+'
+
+addrtoquery() {
+    echo "$1" | perl -e "$PERL_ADDR_TO_QUERY"
+}
+
+host=$(hostname --fqdn)
+
+host $host 8.8.8.8 | awk '/ has .*address/ {print $NF}' | sort >| /tmp/resolved.$$
+(curl --silent -4 http://ifconfig.co; curl --silent -6 http://ifconfig.co) | sort >| /tmp/ifconfig.$$
+comm -12 /tmp/resolved.$$ /tmp/ifconfig.$$ >| /tmp/both.$$
+
+while read addr; do
+    reverse_ip=`addrtoquery "$addr"`
+    while read dnsbl; do
+        if output="$(host -W 5 $reverse_ip.$dnsbl 2>&1)"; then
+            if [ -n "$output" ]; then
+                echo "$host ($addr) is listed in $dnsbl"
+                echo Lookup output:
+                echo "$output"
+                continue
+            fi
+        fi
+        if $verbose; then
+            echo "$host ($addr) is not listed in $dnsbl"
+        fi
+    done < /etc/dnsbls
+done < /tmp/both.$$
+
+rm -f /tmp/*.$$
+
